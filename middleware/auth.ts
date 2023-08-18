5ac6b0b33863bd74f09a16d8bb26ae6cdc221e7b
Kunal Agrawal <kunal.py3@gmail.com>
2023-08-18 21:49:38 +0530

    middleware added

---
+++ middleware/auth.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../controllers/auth";
+
+export async function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // TODO: retrieve and verify given jwt token, if true set it decoded token to req.user. else, return error
+  const token = req.headers.authorization?.split("")[1] || "";
+  try {
+    await verifyToken({ token }).then((decoded) => {
+      // @ts-ignore
+      if (decoded) req.user = decoded;
+      else res.status(401).json({ message: "Token is not valid" });
+    });
+  } catch (err: any) {
+    res.status(401).json({ message: err.message });
+  }
+  next();
+}
