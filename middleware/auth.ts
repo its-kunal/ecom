28101ef22c3c175d40f7aca4755a1ea12b29c169
Kunal Agrawal <kunal.py3@gmail.com>
2023-08-19 12:01:04 +0530

    fix: set headers

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -4,18 +4,23 @@ import { verifyToken } from "../controllers/auth";
 export async function authMiddleware(
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) {
   // TODO: retrieve and verify given jwt token, if true set it decoded token to req.user. else, return error
-  const token = req.headers.authorization?.split("")[1] || "";
+  const token = req.headers.authorization?.split(" ")[1] || "";
+
   try {
     await verifyToken({ token }).then((decoded) => {
-      // @ts-ignore
-      if (decoded) req.user = decoded;
-      else return res.status(401).json({ message: "Token is not valid" });
+      if (decoded === false) {
+        return res.status(401).json({ message: "Token is not valid" });
+      } else {
+        // @ts-ignore
+        req.user = decoded;
+        return next();
+      }
     });
   } catch (err: any) {
-    res.status(401).json({ message: err.message });
+    return res.status(401).json({ message: err.message });
   }
-  next();
+  // next();
 }
