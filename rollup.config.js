00bc7548796873880f8069bd001abc7431bf3dbf
Kunal Agrawal <kunal.py3@gmail.com>
2023-10-27 16:28:13 +0530

    Rollup config file update

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,11 @@
 // rollup.config.mjs
-import path from "path";
-import alias from "@rollup/plugin-alias";
-import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
-const customResolver = resolve({
-  extensions: [".mjs", ".js", ".jsx", ".json", ".sass", ".scss"],
-});
-const projectRootDir = path.resolve(__dirname);
-
 export default {
-  input: "server.js",
+  input: "server.ts",
   output: {
     file: "dist/server.js",
-    format: "mjs",
+    format: "cjs",
   },
-  plugins: [
-    alias({
-      entries: [
-        { find: "@/*", replacement: path.resolve(projectRootDir, "./") },
-      ],
-      customResolver,
-    }),
-    resolve(),
-    typescript(),
-  ],
+  plugins: [typescript()],
 };
